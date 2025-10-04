0504d765dae0e4a09972cd62b1b88d90d9000d99
MiguelFMont <miguel.fermon10@gmail.com>
2025-10-04 16:08:34 -0300

    Adicionado conexão com JS e imagem no <head>

---
+++ scripts/pageCadastro.js
@@ -0,0 +1,6 @@
+function main() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const senha = document.getElementById('senha').value;
+}
